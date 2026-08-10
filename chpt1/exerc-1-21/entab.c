45ebaa9990895f842b495c65ddaeaa43cbffc1b0
winzerprince <aitajprince200@gmail.com>
2026-08-10 11:25:20 +0300

    Added exc 21 still making progress

--- chpt1/exerc-1-21/entab.c
+++ chpt1/exerc-1-21/entab.c
@@ -2,3 +2,31 @@
 // minimum number of tabs and blanks to achieve the same spacing. Use the same
 // tab stops as for detab. When either a tab or a single blank would suffice to
 // reach a tab stop, which should be given preference?
+
+#include <stdio.h>
+#define TAB 8
+
+int get_line(char *s, int lim);
+void detab(char *s, int size);
+
+int main() {}
+
+int get_line(char *s, int lim) {
+  char c;
+  int i = 0;
+  while ((c = getchar()) != EOF && c != '\n') {
+    if (i < lim - 1) {
+      s[i] = c;
+    }
+    i++;
+  }
+
+  if (c == '\n') {
+    s[i] = '\n';
+    i++;
+  }
+  s[i] = '\0';
+  return i + 2;
+}
+
+void detab(char *s, int size) {}
---
+++ chpt1/tmp.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  int a = -3 % 2;
+  int b;
+  int c;
+  printf("a:%d\nb:%d\nc:%d", a, b, c);
+  return 0;
+}
