0fe5cb2f91e3847d9bce76a0193987c18fe7c925
winzerprince <aitajprince200@gmail.com>
2026-09-23 13:24:51 +0300

    feat: stuck on 2-6

--- chpt2/exerc-2-5/strpbrk.c
+++ chpt2/exerc-2-5/strpbrk.c
@@ -1,4 +1,4 @@
-// PENDING
+// COMPLETE
 // Exercise 2-5. Write the function any(s1,s2), which returns the first location
 // in a string s1 where any character from the string s2 occurs, or -1 if s1
 // contains no characters from s2. (The standard library function strpbrk does
--- chpt2/exerc-2-6/setbits.c
+++ chpt2/exerc-2-6/setbits.c
@@ -1,3 +1,20 @@
+// PENDING
 // Exercise 2-6. Write a function setbits(x,p,n,y) that returns x with the n
 // bits that begin at position p set to the rightmost n bits of y, leaving the
 // other bits unchanged
+//
+// Example
+// Assumptions: postions move right to left beginning at 0
+//
+// x = 10100001 (has positions 0 to y from right to left)
+// y = 10100111
+// p = 3
+// n = 2
+//
+// setbits(x,p,n,y) = 10101101
+//
+// the '11' at the right side of y in replace the '00' in postions 3 and 2 of x
+//
+// steps
+//
+//
